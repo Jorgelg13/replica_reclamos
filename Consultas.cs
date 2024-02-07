dcb4082eca7c45be5745f5663a7166238259a6ec
Jorge <jorgelg132012@gmail.com>
2024-02-06 22:09:54 -0600

    cambios

--- Consultas.cs
+++ Consultas.cs
@@ -1,11 +1,17 @@
 ﻿
+using System.Collections.Generic;
+
 namespace ReplicaDataReclamos
 {
     internal class Consultas
     {
+        public static string CADENA_CONEXION()
+        {
+            return "Persist Security Info=False;User ID=sa;Password=admin123;Initial Catalog=reclamos;Server=alienware\\SQLEXPRESS";
+        }
         public static string RECLAMOS_AUTOS()
         {
-            return "select top(100) " +
+            return "select " +
                     "r.id," +
                     "r.boleta," +
                     "r.titular," +
@@ -35,13 +41,13 @@ namespace ReplicaDataReclamos
                     "a.asegurado " +
                     "from reclamo_auto as r " +
                     "inner join auto_reclamo as a on r.id_auto_reclamo = a.id " +
-                    "inner join usuario as u on u.id = r.id_usuario ";
-            //"where CONVERT(DATE,r.fecha_commit,112) = CONVERT(DATE,GETDATE(),112)";
+                    "inner join usuario as u on u.id = r.id_usuario " +
+                    "where r.id_estado = 2 and replica_ibis = 0";
         }
 
         public static string RECLAMOS_VARIOS()
         {
-            return "Select top(100)  " +
+            return "Select " +
                 "r.id," +
                 "r.boleta," +
                 "r.titular," +
@@ -67,8 +73,8 @@ namespace ReplicaDataReclamos
                 "rv.moneda " +
                 "from reclamos_varios as r " +
                 "inner join reg_reclamo_varios as rv on r.id_reg_reclamos_varios = rv.id " +
-                "inner join usuario as u on u.id = r.id_usuario ";
-                //"where CONVERT(DATE,r.fecha_commit,112) = CONVERT(DATE,GETDATE(),112)";
+                "inner join usuario as u on u.id = r.id_usuario " +
+                "where r.id_estado = 2 and replica_ibis = 0";
         }
 
         public static string RECLAMOS_MEDICOS()
@@ -92,38 +98,44 @@ namespace ReplicaDataReclamos
                 "rm.certificado " +
                 "from reclamos_medicos as r " +
                 "inner join reg_reclamos_medicos as rm on rm.id = r.id_reg_reclamos_medicos " +
-                "inner join usuario as u on u.id = r.id_usuario";
-            //"where CONVERT(DATE,r.fecha_commit,112) = CONVERT(DATE,GETDATE(),112)";
+                "inner join usuario as u on u.id = r.id_usuario " +
+                "where r.id_estado = 2 and replica_ibis = 0";
         }
 
-        public static string BITACORA_AUTOS()
+        public static string BITACORA_AUTOS(List<int> ids)
         {
-            return "SELECT top(1000) " +
+            return "SELECT " +
                "id,"+
                "descripcion, " +
                "fecha_commit as fecha_registro, " +
                "hora_commit as hora_registro," +
                "id_reclamo, " +
-               "usuario FROM bitacora_reclamo_auto ";
-            // "WHERE CONVERT(DATE,fecha_commit,112) = CONVERT(DATE,GETDATE(),112)";
+               "usuario FROM bitacora_reclamo_auto " +
+               "WHERE id_reclamo in (" + string.Join(",", ids) + ")";
         }
 
-        public static string BITACORA_RECLAMOS_VARIOS()
+        public static string BITACORA_RECLAMOS_VARIOS(List<int> ids)
         {
-            return "SELECT top(1000) " +
+            return "SELECT " +
                 "id," +
                 "descripcion, " +
                 "fecha_commit as fecha_registro, " +
                 "hora_commit as hora_registro," +
                 "id_reclamos_varios, " +
-                "usuario FROM bitacora_reclamos_varios ";
-            // "WHERE CONVERT(DATE,fecha_commit,112) = CONVERT(DATE,GETDATE(),112)";
+                "usuario FROM bitacora_reclamos_varios " +
+                "WHERE id_reclamos_varios in (" + string.Join(",", ids) + ")";
         }
 
-        public static string RECIBOS_MEDICOS()
+        public static string RECIBOS_MEDICOS(List<int> ids)
         {
-            return "SELECT top(1000) * FROM recibos_medicos ";
-            // "WHERE CONVERT(DATE,fecha_commit,112) = CONVERT(DATE,GETDATE(),112)";
+            return "SELECT " +
+                "id, " +
+                "descripcion as tipo_documento, " +
+                "comentarios, " +
+                "cantidad, " +
+                "id_reclamo_medico " +
+                "FROM recibos_medicos " +
+                "WHERE id_reclamo_medico in (" + string.Join(",", ids) + ")";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace ReplicaDataReclamos
 {
@@ -12,20 +15,9 @@ namespace ReplicaDataReclamos
             try
             {
                 string basePath = @"C:\FTP\Guatemala\Reclamos\Cabina\";
-                ExportReclamos(
-                    getData(Consultas.RECLAMOS_AUTOS()),
-                    getData(Consultas.BITACORA_AUTOS()),
-                    basePath + "reclamos_autos.xlsx");
-
-                ExportReclamos(
-                    getData(Consultas.RECLAMOS_VARIOS()),
-                    getData(Consultas.BITACORA_RECLAMOS_VARIOS()),
-                    basePath + "reclamos_varios.xlsx");
-
-                ExportReclamos(
-                    getData(Consultas.RECLAMOS_MEDICOS()),
-                    getData(Consultas.RECIBOS_MEDICOS()),
-                    basePath + "reclamos_medicos.xlsx");
+                ExportReclamos(getData(Consultas.RECLAMOS_AUTOS()),basePath + "reclamos_autos.xlsx","reclamo_auto");
+                ExportReclamos(getData(Consultas.RECLAMOS_VARIOS()),basePath + "reclamos_varios.xlsx","reclamos_varios");
+                ExportReclamos(getData(Consultas.RECLAMOS_MEDICOS()),basePath + "reclamos_medicos.xlsx","reclamos_medicos");
             }
             catch (Exception ex)
             {
@@ -35,13 +27,11 @@ namespace ReplicaDataReclamos
 
         public static DataTable getData(string query)
         {
-            string connectionString = "Persist Security Info=False;User ID=sa;Password=admin123;Initial Catalog=reclamos;Server=alienware\\SQLEXPRESS";
-
             try
             {
                 DataTable dataTable = new DataTable();
 
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(Consultas.CADENA_CONEXION()))
                 {
                     connection.Open();
 
@@ -65,41 +55,94 @@ namespace ReplicaDataReclamos
             }
         }
 
-        static void ExportReclamos(DataTable reclamosAutos, DataTable bitacora, string filePath)
+        static void ExportReclamos(DataTable reclamos, string filePath, string tipo)
         {
+            DataTable bitacora = null;
+            List<int> ids = new List<int>();
+
             using (var workbook = new XLWorkbook())
             {
                 var worksheet = workbook.Worksheets.Add("reclamos");
 
-                for (int i = 0; i < reclamosAutos.Columns.Count; i++)
+                for (int i = 0; i < reclamos.Columns.Count; i++)
                 {
-                    worksheet.Cell(1, i + 1).Value = reclamosAutos.Columns[i].ColumnName;
+                    worksheet.Cell(1, i + 1).Value = reclamos.Columns[i].ColumnName;
                 }
 
-                for (int i = 0; i < reclamosAutos.Rows.Count; i++)
+                for (int i = 0; i < reclamos.Rows.Count; i++)
                 {
-                    for (int j = 0; j < reclamosAutos.Columns.Count; j++)
+                    for (int j = 0; j < reclamos.Columns.Count; j++)
                     {
-                        worksheet.Cell(i + 2, j + 1).Value = reclamosAutos.Rows[i][j].ToString();
+                        worksheet.Cell(i + 2, j + 1).Value = reclamos.Rows[i][j].ToString();
+
+                        if (j == 0)
+                        {
+                            ids.Add(Convert.ToInt32(reclamos.Rows[i][j]));
+                        }
                     }
+                }
+
+                if(tipo == "reclamo_auto" && ids.Count > 0)
+                {
+                    bitacora = getData(Consultas.BITACORA_AUTOS(ids));
+                } 
+                
+                else if(tipo == "reclamos_varios" && ids.Count > 0)
+                {
+                    bitacora = getData(Consultas.BITACORA_RECLAMOS_VARIOS(ids));
+                } 
+                
+                else if(tipo == "reclamos_medicos" && ids.Count > 0)
+                {
+                    bitacora = getData(Consultas.RECIBOS_MEDICOS(ids));
                 }
 
                 var worksheetTwo = workbook.Worksheets.Add("bitacoras");
-                for (int i = 0; i < bitacora.Columns.Count; i++)
-                {
-                    worksheetTwo.Cell(1, i + 1).Value = bitacora.Columns[i].ColumnName;
-                }
 
-                for (int i = 0; i < bitacora.Rows.Count; i++)
+                if (ids.Count > 0)
                 {
-                    for (int j = 0; j < bitacora.Columns.Count; j++)
+                    for (int i = 0; i < bitacora.Columns.Count; i++)
                     {
-                        worksheetTwo.Cell(i + 2, j + 1).Value = bitacora.Rows[i][j].ToString();
+                        worksheetTwo.Cell(1, i + 1).Value = bitacora.Columns[i].ColumnName;
+                    }
+
+                    for (int i = 0; i < bitacora.Rows.Count; i++)
+                    {
+                        for (int j = 0; j < bitacora.Columns.Count; j++)
+                        {
+                            worksheetTwo.Cell(i + 2, j + 1).Value = bitacora.Rows[i][j].ToString();
+                        }
                     }
                 }
 
-                // Guardar el archivo Excel
                 workbook.SaveAs(filePath);
+
+                if (ids.Count >0)
+                {
+                    updateRegistros(tipo, ids);
+                }
+            }
+        }
+
+        static void updateRegistros(string tabla, List<int> ids)
+        {
+            string query = "UPDATE " + tabla + " SET replica_ibis = 1 WHERE id in (" + string.Join(",", ids) + ")";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Consultas.CADENA_CONEXION()))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        int filasActualizadas = command.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al actualizar el registro: " + ex.Message);
             }
         }
     }
